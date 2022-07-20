afe7fcd5d86f87b36676e54cd0acf034a877934c
Sahim Salem <61041330+Sahim98@users.noreply.github.com>
2022-07-20 09:39:08 +0600

    Create Sum of Two Values.cpp

---
+++ sorting & searching/Sum of Two Values.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MOD = 1000000007;
+using ld = long double;
+using ll = long long;
+using vi = vector<int> ;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using si = set<int> ;
+using sll = set<ll> ;
+using vll = vector<ll> ;
+using mii = map<int,int> ;
+using mll = map<ll,ll> ;
+using vpii = vector<pair<int,int>> ;
+using vvi = vector<vector<int>> ;
+ 
+#define endl '\n'
+#define F first
+#define S second
+#define pb push_back
+#define br cout<<'\n'; 
+#define inf 0x3f3f3f3f
+#define infll 0x3f3f3f3f3f3f3f3fLL
+#define lcm(a, b) a /__gcd(a, b)*b
+#define digit(n) floor(log10(n))+1
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define case(x) cout<< "Case "<<x<< ": ";
+#define ver(expression) cout << (expression ? "YES\n" : "NO\n")
+#define fast {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define prec(precision,value) cout<<fixed<<setprecision(precision)<<value<<endl
+ 
+ 
+#define bug(args...) do {cerr << #args << " : "; dbg(args); } while(0)
+void dbg () {cerr << endl;}
+template <typename T>
+void dbg( T a[], int n ) {for(int i = 0; i < n; ++i) cerr << a[i] << ' ';cerr << endl;}
+template <typename T, typename ... hello>
+void dbg( T arg, const hello &... rest) {cerr << arg << ' '; dbg(rest...);}
+ 
+ 
+ 
+template <typename T>ostream& operator<<(ostream& os, const vector<T>& v){
+os << "[";for (int i = 0; i < v.size(); ++i) {os << v[i];
+if (i != v.size() - 1)os << ", ";}os << "]\n";return os;}
+ 
+template <typename T>ostream& operator<<(ostream& os, const set<T>& v){
+os << "[";for (auto it : v) {os << it;if (it != *v.rbegin())os << ", ";}
+os << "]\n";return os;}
+ 
+ 
+ 
+void solve()
+{
+    int n ,x ; 
+    cin >> n >> x ;
+    vi a(n) ; map<int,vi> pos ;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i] ;
+        pos[a[i]].pb(i) ;
+    }
+    
+    sort(all(a)) ; 
+ 
+    int i = 0 , j = n - 1 ;
+ 
+    while (i< j)
+    {
+        if (a[i] + a[j] < x)
+        {
+            i++ ;
+        }
+        else if (a[i] + a[j] > x)
+        {
+            j-- ;
+        }
+        else
+        {
+           break;
+        }
+    }
+ 
+    if(i == j || a[i] + a[j] != x) cout << "IMPOSSIBLE" << endl ;
+ 
+ 
+ 
+    else{
+ 
+        if(a[i] == a[j]){
+            cout << pos[a[i]][0] + 1 << " " << pos[a[j]][1] + 1 << endl ;
+        }
+ 
+        else cout << pos[a[i]][0]+1 << " " << pos[a[j]][0]+1 << endl ;
+    }
+    
+    
+    
+}
+ 
+int main()
+{
+    fast
+    int test = 1 ; //cin >> test ; 
+    for(int i = 1;  i <= test ; i++)solve() ;  
+}
